92a6892454dbac67e1f4f61c7e8f6f615e20c9bc
Ali Unlu <33809587+aliunlu97@users.noreply.github.com>
2019-12-14 00:36:19 +0000

    LMCNet, MCNet, and MLMCNet done. TSCNN is WIP

---
+++ FCN.py
@@ -0,0 +1,69 @@
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+
+
+class FullyConvNet(nn.Module):
+    def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
+        super().__init__()
+        self.input_shape = ImageShape(height=height, width=width, channels=channels)
+        self.class_count = class_count
+
+        self.dropout=nn.Dropout(p=dropout)
+
+        self.conv1 = nn.Conv2d(
+            in_channels=self.input_shape.channels,
+            out_channels=32,
+            kernel_size=(3, 3),
+            padding=(1, 1),
+        )
+        self.conv1_bn = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=(3, 3),
+            padding=(1, 1),
+        )
+        self.conv2_bn = nn.BatchNorm2d(32)
+
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+
+        self.conv3 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(3, 3),
+            padding=(1, 1),
+        )
+        self.conv3_bn =nn.BatchNorm2d(64)
+
+        self.conv4 = nn.Conv2d(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(3, 3),
+            padding=(1, 1),
+            stride=(2, 2),
+        )
+        self.conv4_bn =nn.BatchNorm2d(64)
+
+        self.initialise_layer(self.conv1)
+        self.initialise_layer(self.conv2)
+        self.initialise_layer(self.conv3)
+        self.initialise_layer(self.conv4)
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x=F.relu(self.conv1_bn(self.conv1(x)))
+        x=F.relu(self.conv2_bn(self.conv2(x)))
+        x=self.pool(x)
+        x=F.relu(self.conv3_bn(self.conv3(x)))
+        x=F.relu(self.conv4_bn(self.conv4(x)))
+
+        return x
+
+    @staticmethod
+    def initialise_layer(layer):
+        if hasattr(layer, "bias"):
+            nn.init.zeros_(layer.bias)
+        if hasattr(layer, "weight"):
+            nn.init.kaiming_normal_(layer.weight)
--- model.py
+++ model.py
@@ -1,79 +1,85 @@
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
+from FCN import FullyConvNet
 
-
-class CNN(nn.Module):
-    def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
-        super().__init__()
-        self.input_shape = ImageShape(height=height, width=width, channels=channels)
-        self.class_count = class_count
-
-        self.dropout=nn.Dropout(p=dropout)
-
-        self.conv1 = nn.Conv2d(
-            in_channels=self.input_shape.channels,
-            out_channels=32,
-            kernel_size=(3, 3),
-            padding=(1, 1),
-        )
-        self.conv1_bn = nn.BatchNorm2d(32)
-
-        self.conv2 = nn.Conv2d(
-            in_channels=32,
-            out_channels=32,
-            kernel_size=(3, 3),
-            padding=(1, 1),
-        )
-        self.conv2_bn = nn.BatchNorm2d(32)
-
-        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-
-        self.conv3 = nn.Conv2d(
-            in_channels=32,
-            out_channels=64,
-            kernel_size=(3, 3),
-            padding=(1, 1),
-        )
-        self.conv3_bn =nn.BatchNorm2d(64)
-
-        self.conv4 = nn.Conv2d(
-            in_channels=64,
-            out_channels=64,
-            kernel_size=(3, 3),
-            padding=(1, 1),
-            stride=(2, 2),
-        )
-        self.conv4_bn =nn.BatchNorm2d(64)
+class LMCNet(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+        super(LMCNet, self).__init__()
+        self.fcn = FullyConvNet(input_size, num_channels, kernel_size, dropout=dropout)
 
         self.fc1 = nn.Linear(15488, 1024)
         # self.fc1_bn =nn.BatchNorm1d(1024)
         self.fc2 = nn.Linear(1024, 10)
 
-        self.initialise_layer(self.conv1)
-        self.initialise_layer(self.conv2)
-        self.initialise_layer(self.conv3)
-        self.initialise_layer(self.conv4)
+        self.initialise_layer(self.fc1)
+        self.initialise_layer(self.fc2)
+
+    def forward(self, x):
+        x=self.fcn(x)
+        x=torch.flatten(x,start_dim=1)
+        x=F.sigmoid(self.fc1(x))
+        x=F.softmax(self.fc2(x))
+
+        return x
+
+class MCNet(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+        super(MCNet, self).__init__()
+        self.fcn = FullyConvNet(input_size, num_channels, kernel_size, dropout=dropout)
+
+        self.fc1 = nn.Linear(15488, 1024)
+        # self.fc1_bn =nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 10)
 
         self.initialise_layer(self.fc1)
         self.initialise_layer(self.fc2)
 
-    def forward(self, images: torch.Tensor) -> torch.Tensor:
-        x=F.relu(self.conv1_bn(self.conv1(images)))
-        x=F.relu(self.conv2_bn(self.conv2(x)))
-        x=self.pool(x)
-        x=F.relu(self.conv3_bn(self.conv3(x)))
-        x=F.relu(self.conv4_bn(self.conv4(x)))
-
+    def forward(self, x):
+        x=self.fcn(x)
         x=torch.flatten(x,start_dim=1)
         x=F.sigmoid(self.fc1(x))
         x=F.softmax(self.fc2(x))
-        
+
         return x
 
-    @staticmethod
-    def initialise_layer(layer):
-        if hasattr(layer, "bias"):
-            nn.init.zeros_(layer.bias)
-        if hasattr(layer, "weight"):
-            nn.init.kaiming_normal_(layer.weight)
+class MLMCNet(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+        super(MLMCNet, self).__init__()
+        self.fcn = FullyConvNet(input_size, num_channels, kernel_size, dropout=dropout)
+
+        self.fc1 = nn.Linear(49728, 1024)
+        # self.fc1_bn =nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 10)
+
+        self.initialise_layer(self.fc1)
+        self.initialise_layer(self.fc2)
+
+    def forward(self, x):
+        x=self.fcn(x)
+        x=torch.flatten(x,start_dim=1)
+        x=F.sigmoid(self.fc1(x))
+        x=F.softmax(self.fc2(x))
+
+        return x
+
+# WORK IN PROGRESS
+# class TSCNN(nn.Module):
+#     def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+#         super(TSCNN, self).__init__()
+#         self.fcn = FullyConvNet(input_size, num_channels, kernel_size, dropout=dropout)
+#
+#         self.fc1 = nn.Linear(15488, 1024)
+#         # self.fc1_bn =nn.BatchNorm1d(1024)
+#         self.fc2 = nn.Linear(1024, 10)
+#
+#         self.initialise_layer(self.fc1)
+#         self.initialise_layer(self.fc2)
+#
+#     def forward(self, x):
+#         x=self.fcn(x)
+#         x=torch.flatten(x,start_dim=1)
+#         x=F.sigmoid(self.fc1(x))
+#         x=F.softmax(self.fc2(x))
+#
+#         return x
