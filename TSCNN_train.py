c3661fdff09699be4c726770d877e29040ecef23
Ali Unlu <33809587+aliunlu97@users.noreply.github.com>
2019-12-27 09:00:53 +0000

    No clone

--- TSCNN_train.py
+++ TSCNN_train.py
@@ -267,8 +267,8 @@ class Trainer:
                 self.optimizer.step()
                 self.optimizer.zero_grad()
 
-                logits_array=logits.clone().detach().cpu().numpy()
-                labels_array=labels.clone().detach().cpu().numpy()
+                logits_array=logits.detach().cpu().numpy()
+                labels_array=labels.detach().cpu().numpy()
 
                 # with torch.no_grad():
                 #     preds = logits.argmax(-1)
